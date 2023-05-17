2a4614cb8309920435fff9f0d790b2427bdaada3
Anderson Motta <anderson.motta@tegra.com.br>
2023-05-17 12:17:13 -0300

    replace and remane login image

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ export class Login {
     static callback_naook = null;
     static config = {
         cor: "048",
-        img: "logo.png"
+        img: "login.jfif"
     };
 
     static endpoint = "https://loginv1.cfbcursos.repl.co/";
@@ -22,9 +22,9 @@ export class Login {
         this.estilocss =
             ".fundoLogin {display: flex;justify-content: center;align-items: center;width: 100%;height: 100vh;position: absolute;top: 0px;left: 0px;background-color: rgba(0, 0, 0, 0.75);box-sizing: border-box;}" +
             ".baseLogin {display: flex;justify-content: center;align-items: stretch;width: 40%;box-sizing: inherit;}" +
-            ".elementosLogin {display: flex;justify-content: center;align-items: flex-start;flex-direction: column;width: 50%;background-color: #ccc;padding: 10px;border-radius: 10px 0px 0px 10px;box-sizing: inherit;}" +
             ".logoLogin {display: flex;justify-content: center;align-items: center;width: 50%;background-color: #ccc;padding: 10px;border-radius: 0px 10px 10px 0px;box-sizing: inherit;}" +
             ".logoLogin img {width: 90%;box-sizing: inherit;}" +
+            ".elementosLogin {display: flex;justify-content: center;align-items: flex-start;flex-direction: column;width: 50%;background-color: #ccc;padding: 10px;border-radius: 10px 0px 0px 10px;box-sizing: inherit;}" +
             ".campoLogin {display: flex;justify-content: flex-start;align-items: flex-start;flex-direction: column;box-sizing: inherit;margin-bottom: 10px;}" +
             ".campoLogin label {font-size: 18px;}" +
             ".campoLogin input {font-size: 18px;padding: 5px;background-color: #fff;border-radius: 5px;}" +
