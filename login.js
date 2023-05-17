cf5e5ed5d25158d26d9fc86919116d6cec2a479f
Anderson Motta <anderson.motta@tegra.com.br>
2023-05-17 12:06:38 -0300

    adjust color and image

--- login.js
+++ login.js
@@ -21,14 +21,14 @@ export class Login {
         this.callback_naook = () => { callback_naook() };
         this.estilocss =
             ".fundoLogin {display: flex;justify-content: center;align-items: center;width: 100%;height: 100vh;position: absolute;top: 0px;left: 0px;background-color: rgba(0, 0, 0, 0.75);box-sizing: border-box;}" +
-            ".baseLogin {display: flex;justify-content: center;align-items: stretch;width: 60%;box-sizing: inherit;}" +
+            ".baseLogin {display: flex;justify-content: center;align-items: stretch;width: 40%;box-sizing: inherit;}" +
             ".elementosLogin {display: flex;justify-content: center;align-items: flex-start;flex-direction: column;width: 50%;background-color: #ccc;padding: 10px;border-radius: 10px 0px 0px 10px;box-sizing: inherit;}" +
-            ".logoLogin {display: flex;justify-content: center;align-items: center;width: 50%;background-color: #bbb;padding: 10px;border-radius: 0px 10px 10px 0px;box-sizing: inherit;}" +
+            ".logoLogin {display: flex;justify-content: center;align-items: center;width: 50%;background-color: #ccc;padding: 10px;border-radius: 0px 10px 10px 0px;box-sizing: inherit;}" +
             ".logoLogin img {width: 90%;box-sizing: inherit;}" +
             ".campoLogin {display: flex;justify-content: flex-start;align-items: flex-start;flex-direction: column;box-sizing: inherit;margin-bottom: 10px;}" +
             ".campoLogin label {font-size: 18px;}" +
             ".campoLogin input {font-size: 18px;padding: 5px;background-color: #fff;border-radius: 5px;}" +
-            ".botoesLogin {display: flex;justify-content: space-around;align-items: center;width: 100%;box-sizing: inherit;}" +
+            ".botoesLogin {display: flex;justify-content: space-evenly;align-items: center;width: 100%;box-sizing: inherit;}" +
             `.botoesLogin button {cursor: pointer;background-color:#${this.config.cor};color: #fff;border-radius: 5px;padding: 10px;width: 100px;box-sizing: inherit;}`
 
         const styleEstilo = document.createElement("style");
